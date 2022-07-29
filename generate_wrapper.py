07d3a42750b8a49c990dd7695108ee49057a088a
brunbruns <104069568+brunbruns@users.noreply.github.com>
2022-07-29 10:23:19 +0200

    Add GUI and correction of redaction issue

--- generate_wrapper.py
+++ generate_wrapper.py
@@ -9,7 +9,7 @@ def wrap(wrapper_path, top_path , clk_name):
     wrapper_name= wrapper_name.split(".")[0] #supress .vhd
 
     top_name= top_path.split("/")[-1]#will be use for wrapper generation   
-    name= top_name.split(".")[0] #supress .vhd
+    top_name= top_name.split(".")[0] #supress .vhd
     
 
 
@@ -18,18 +18,18 @@ def wrap(wrapper_path, top_path , clk_name):
     type =[] #list of string with port type
     identify_component= False #boolean to knwo if compent as been found
     identify_end = False #boolean to know that all port have been found 
-
-    file_r = open(top_path, 'r') #open in read mode (no need to change) 
+    try:
+        file_r = open(top_path, 'r') #open in read mode (no need to change) 
+    except:
+        return("error top path is invalid")
 
     ################### read the file to identify port
-    print(f"\n*Start reading {top_name}\n")
+    
     while not identify_component or not identify_end:
         line = file_r.readline()
         
         if not line:##in case of file is read till the end and programme failed 
-            print(f"** ERROR file ended without entity declaration found ************" )
-            print(f"->    Start of declaration found: {identify_component}  " )
-            print(f"->    End of declaration found:{identify_end}  " )
+            
             break#end of while 
         line = line.lower() #VHDL doesn't have a specific case sensitivity 
         
@@ -40,7 +40,7 @@ def wrap(wrapper_path, top_path , clk_name):
                 line = line.replace("entity","") #replace is better than strip, strip only remove at start and end of a str 
                 line = line.replace(" ","") #remove spaces  
                 entity_name=line #name of the entity 
-                print ("entity " + entity_name + " identified") 
+                
         
         elif identify_component: #if component is identified, look for port 
             
@@ -48,13 +48,7 @@ def wrap(wrapper_path, top_path , clk_name):
                 identify_end = True #stop the reading loop  
 
                 type[-1]=type[-1][:-1]#supress last char which was a ')' closing entity declaration  
-                print(f"\n{len(port)}  ports:  ")
-                print(port)
-                print("direction:") 
-                print(direction)   
-                print("type:") 
-                print(type)  
-                print ("\nend of entity")
+                
             
             elif line.find(":") != -1: #according to "standard" writing name of port should be before those :
                 
@@ -62,7 +56,7 @@ def wrap(wrapper_path, top_path , clk_name):
                 
                 #find port name
                 line0=line[0] 
-                if line0.find("signal") != -1 : # if end sequence isn't detexted signal could be seen as port, however if port name contain signal only a warning is displayed 
+                if line0.find("signal") != -1 : # if end sequence isn't detected signal could be seen as port, however if port name contain signal only a warning is displayed 
                     print("**Warning: line with signal keyword take as a port**")
                 line0=line0.replace('port','')
                 line0=line0.replace('(','')
@@ -85,6 +79,15 @@ def wrap(wrapper_path, top_path , clk_name):
     
     file_r.close()
 
+    #verify that clock port and clk_name are the same
+    check_clk_name=0
+    for k in range(len(port)):
+        if port[k]==clk_name:
+            check_clk_name = 1
+    
+    if check_clk_name == 0 :
+        return (f"error clock port {clk_name} not found ")
+
      
     ### here declaration of usefull string, could be stored in a json but still storing in the very own python code isn't a bad practice here
     str_import="library IEEE;\nuse IEEE.STD_LOGIC_1164.ALL;\n\n" 
@@ -96,16 +99,18 @@ def wrap(wrapper_path, top_path , clk_name):
     #################################### generate vhdl wrapper  
 
     if identify_component and identify_end:#only generate wrapper if source file was read with sucess 
-        print(f"\n\n\n*Start editing {wrapper_name}\n")
-
-        file_w= open(wrapper_path, 'w') #here write mode needed (also overwrite in case file existe)  
-
+        
+        try:
+            file_w= open(wrapper_path, 'w') #here write mode needed (also overwrite in case file existe)  
+        except:
+            return("error wrapper path is invalid")
         file_w.write(str_import)
         
         file_w.write("entity "+wrapper_name +  " is\n")
         file_w.write("\tPort(\n")
         for k in range(len(port)):#delclaration of entity 
-            if port[k].find(clk_name) == -1: #clk port has to be removed at the top of the wrapper 
+            if port[k]!=clk_name: #clk port has to be removed at the top of the wrapper 
+                
                 if k< len(port)-1:
                     file_w.write( "\t\t" + port[k]+ " : " + direction[k] + " " + type[k]+ ";\n"   )
                 else:
@@ -131,7 +136,7 @@ def wrap(wrapper_path, top_path , clk_name):
         
         file_w.write("\ttoppm: "+ top_name + " port map(\n")
         for k in range(len(port)):#delclaration of entity 
-            if port[k].find(clk_name) == -1: #clk port has to be removed at the top of the wrapper 
+            if port[k] == clk_name: #clk port has to be removed at the top of the wrapper 
                 if k< len(port)-1:
                     file_w.write( "\t\t" + port[k] + "=>"+ " " + port[k]+ ",\n"   )
                 else:
@@ -143,6 +148,18 @@ def wrap(wrapper_path, top_path , clk_name):
                     file_w.write( "\t\t" + port[k] + "=>"+ " " + "s_clk"+ ");\n"   ) #last line is different
         file_w.write("\n\nend struct;")  
         file_w.close()
-        print(f"*Closing {wrapper_name}\n")
+        
+        ports=""
+        for k in range(len(port)):
+            ports=ports+port[k]+"  "
+
+        return(f"wrapper generated: {len(port)} ports identified\n"+ports)
+    
+    else:
+        if not identify_component:
+            return("error entity declaration not found during top reading")
+        if not identify_end:
+            return("error entity end marker not found during top reading")
     print("\n*****************************************************************")
 
+    
--- main.py
+++ main.py
@@ -1,27 +1,107 @@
 import os
 from generate_wrapper import *
+from tkinter import *
+from tkinter.filedialog import askopenfile
+from tkinter.filedialog import asksaveasfile
 
+
+#note to execute with python3 to have tk
+ 
 print("\n\n*********************** Wrapper Generator ***********************\n***************************************************************** \n")
 
-top_path = "/home/t0264060/stage_cocotb_2022/library_perso/generate_wrapper/hdl_basic_demo/mul.vhd" #enter here the vhdl top on which wrapper will be generated 
-wrapper_path= "/home/t0264060/stage_cocotb_2022/library_perso/generate_wrapper/hdl_basic_demo/wrapped_mul.vhd" #enter here the path, with name where the wrapper should be generated
+top_path = "/home/t0264060/stage_cocotb_2022/library_perso/generate_wrapper/hdl/mul.vhd" #enter here the vhdl top on which wrapper will be generated 
+wrapper_path= "/home/t0264060/stage_cocotb_2022/library_perso/generate_wrapper/hdl/wrapped_mul.vhd" #enter here the path, with name where the wrapper should be generated
 clk_name="clk" #the name of the clk port is declared here 
 
-edit_shell=0
-if edit_shell == 1: #change value to enable to enter paths in shell, either path can be changed in the code  
+edit_shell=3
+if edit_shell == 0:  #execute with value harcoded here
+    wrap(wrapper_path, top_path , clk_name)
+elif edit_shell == 1: #change value to enable to enter paths in shell, either path can be changed in the code  
     #ask value to user
     entry=input(f"\ttop_path is {top_path} type a new or press enter\n-->")
     if entry != "": #keep the default value if no entry 
-        if os.path.exists(entry):
-            top_path = entry
-        else: #check the validity of the path
-            print("***ERROR: unvalid path")
-            quit() 
+        top_path = entry
+        
     entry=input(f"\twrapper_path is {wrapper_path} type a new or press enter\n-->")
     if entry != "": #keep the default value if no entry 
         wrapper_path = entry
     entry=input(f"\tclock port name on top is {clk_name} type a new or press enter\n-->")
     if entry != "": #keep the default value if no entry 
         clk_name = entry
+    print("launching function:\n")
+    print("***********************")
+    print(wrap(wrapper_path, top_path , clk_name))
 
-wrap(wrapper_path, top_path , clk_name)
+else : #in other case open a GUI 
+
+    # create root window
+    root = Tk()
+    root.geometry('1000x320')
+    w=1000#define width and heigth 
+    h=320
+
+    # root window title and dimension
+    root.resizable(False, False)
+    root.title("VHDL Wrapper Generator GUI")
+
+    # all widgets will be here
+    #first create button to launch wrapper generator  
+    def wrap_onClick():
+        #on click the 2 path are read and clk
+        top_path=edit_top_path.get(1.0, 'end') 
+        wrapper_path=edit_wrapper_path.get(1.0, 'end')
+        clk_name = edit_clk_name.get(1.0, 'end')
+        top_path= top_path.replace('\n','') #remove enter 
+        wrapper_path= wrapper_path.replace('\n','')
+        clk_name = clk_name.replace('\n','')
+        #launch fun 
+        result= wrap(wrapper_path, top_path , clk_name)
+        label_result.config(text = result)  #display result 
+        if result.find("error") != -1:
+           #if error set text in red 
+            label_result.config(fg = 'red')
+        else: 
+            label_result.config(fg = 'blue') #case were thing went wrong first         
+    button = Button(root, text='Generate',command=wrap_onClick).place(x=0.815*w, y=0.85*h)
+
+    box_width= 120
+    #create an editText for top_path
+    label_top_instruction = Label(root, text='Enter the top path here')
+    label_top_instruction.place(x=0.03*w, y=0.09*h)
+    edit_top_path = Text(root, height=1, width=box_width)
+    edit_top_path .place(x=0.05*w, y=0.15*h)
+    edit_top_path.config(bg='white')
+    #create botton for file explorer for top path
+    def topFile():
+        file = askopenfile(mode ='r', filetypes =[('VHDL file', '*.vhd')]) 
+        edit_top_path.delete(1.0,"end")
+        edit_top_path.insert(1.0, file.name)
+    buttonTop = Button(root, text='open',width= 5,command=topFile).place(x=0.92*w, y=0.14*h)
+    #create an editText for wrapper_path
+    label_wrapper_instruction = Label(root, text='Enter a path for the wrapper here')
+    label_wrapper_instruction.place(x=0.03*w, y=0.29*h)
+    edit_wrapper_path = Text(root, height=1, width=box_width)
+    edit_wrapper_path .place(x=0.05*w, y=0.35*h)
+    edit_wrapper_path.config(bg='white')
+    #create botton for file explorer for wrapper path
+    def wrapFile():
+        file = asksaveasfile(mode ='w', filetypes =[('VHDL file', '*.vhd')]) 
+        edit_wrapper_path.delete(1.0,"end")
+        edit_wrapper_path.insert(1.0, file.name)
+    buttonWrapper = Button(root, text='save as',command=wrapFile, width= 5).place(x=0.92*w, y=0.34*h)
+    #clock name
+    label_wrapper_instruction = Label(root, text='Enter the top clock port name here')
+    label_wrapper_instruction.place(x=0.03*w, y=0.49*h)
+    edit_clk_name = Text(root, height=1, width=box_width)
+    edit_clk_name.place(x=0.05*w, y=0.55*h)
+    edit_clk_name.config(bg='white') 
+    #label for result 
+    label_result = Label(root, text='')
+    label_result.place(x=0.03*w, y=0.70*h)
+    label_result.config(fg = 'blue')
+    
+     
+
+
+    # Execute Tkinter
+    root.mainloop()
